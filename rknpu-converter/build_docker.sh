1dedfdbf281ddb5795dd79e96422f64991ed6740
Sinitame <sinitame@protonmail.com>
2024-01-02 12:28:17 -0400

    Update docker creation script

--- rknpu-converter/build_docker.sh
+++ rknpu-converter/build_docker.sh
@@ -8,9 +8,9 @@ ASSETS_DIR="$(pwd)/assets"
 if ! [ -d $ASSETS_DIR ]; then
     mkdir $ASSETS_DIR
 		echo "Downloading Docker file to $ASSETS_DIR .."
-		wget -q https://raw.githubusercontent.com/rockchip-linux/rknn-toolkit2/v${VERSION}/docker/docker_file/ubuntu_20_04_cp38/Dockerfile_ubuntu_20_04_for_cp38 -P $ASSETS_DIR
+		wget -q https://raw.githubusercontent.com/rockchip-linux/rknn-toolkit2/v${VERSION}/rknn-toolkit2/docker/docker_file/ubuntu_20_04_cp38/Dockerfile_ubuntu_20_04_for_cp38 -P $ASSETS_DIR
 		echo "Downloading converter Python wheel to $ASSETS_DIR .."
-		wget -q https://raw.githubusercontent.com/rockchip-linux/rknn-toolkit2/v${VERSION}/docker/docker_file/ubuntu_20_04_cp38/rknn_toolkit2-1.6.0+81f21f4d-cp38-cp38-linux_x86_64.whl -P $ASSETS_DIR
+		wget -q https://raw.githubusercontent.com/rockchip-linux/rknn-toolkit2/v${VERSION}/rknn-toolkit2/docker/docker_file/ubuntu_20_04_cp38/rknn_toolkit2-1.6.0+81f21f4d-cp38-cp38-linux_x86_64.whl -P $ASSETS_DIR
 		echo "Patch Dockerfile .."
 		patch -u $ASSETS_DIR/Dockerfile_ubuntu_20_04_for_cp38  -i ./dockerfile.patch
 fi
